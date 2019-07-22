e1b55bd26dbfa633a1002ad92b2eee7d914df454
Fujie Wang <zqgraph@gmail.com>
2019-07-22 11:31:49 -0500

    IsoRank code added

--- code/playground4.py
+++ code/playground4.py
@@ -3,10 +3,10 @@ import networkx as nx
 import random
 import utility as uti
 from collections import defaultdict
-from docplex.mp.constants import ComparisonType
-from docplex.mp.model import Model
+# from docplex.mp.constants import ComparisonType
+# from docplex.mp.model import Model
 import math
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 # ------------------------
 #       Parameteres      -
@@ -46,10 +46,10 @@ g2 = nx.relabel_nodes(g1, gt_mapping)
 C = nx.to_numpy_matrix(g1, dtype = np.int64)
 D = nx.to_numpy_matrix(g2, g1_node_list, dtype = np.int64)
 
-# for i in range(len(g2)):
-#     for j in range(len(g2)):
-#         if D[i, j] == 0:
-#             D[i, j] = d_replace
+for i in range(len(g2)):
+    for j in range(len(g2)):
+        if D[i, j] == 0:
+            D[i, j] = d_replace
 
 # The size of two networks will be differnt for real networks
 g1_size = g2_size = network_size
@@ -59,114 +59,146 @@ g1_size = g2_size = network_size
 # ------------------------------
 S = np.ones((g1_size, g2_size), np.float64)
 
-# # ------------------------------
-# #     Initial Greedy Vector    -
-# # ------------------------------
-# b = np.ones(((g1_size + g2_size), 1), np.float64)
+# ------------------------------
+#     Initial Greedy Vector    -
+# ------------------------------
+b = np.ones(((g1_size + g2_size), 1), np.float64)
 
-# #------------------------------------------------------------------------#
-# #                         Iteration starts here                         #
-# #------------------------------------------------------------------------#
-# for _ in range(max_iter):
-# 	print("one iteration done")
-# 	# ------------------------
-# 	#       new S and b      -
-# 	# ------------------------
-# 	b_new = np.ones(((g1_size + g2_size), 1), np.float64)
-# 	b_new = np.negative(b_new)
+#------------------------------------------------------------------------#
+#                         Iteration starts here                         #
+#------------------------------------------------------------------------#
+for _ in range(max_iter):
+	# ------------------------
+	#       new S and b      -
+	# ------------------------
+	b_new = np.ones(((g1_size + g2_size), 1), np.float64)
+	b_new = np.negative(b_new)
 	
-# 	S_new = np.zeros((g1_size, g2_size), np.float64) # the only time we use S_new is assignment, therefore, its initial value doesn't matter
+	S_new = np.zeros((g1_size, g2_size), np.float64) # the only time we use S_new is assignment, therefore, its initial value doesn't matter
 
-# 	# NOTE: for b and b_new, the index of u is u + g1_size
-# 	for i in g1.nodes():
-# 		for u in g2.nodes():
-# 			# ---------------------------------------
-# 			#    The complete bipartite graph       -
-# 			# ---------------------------------------
-# 			# Instead of constructing the actural graph (which is costly), we create a dictionary B with the format:
-# 			# {(j, v) : weight}
-# 			# the 0 means this edge has not been deleted
-# 			B = defaultdict(lambda : 0.0)
-# 			neighbors_of_i = {}
-# 			neighbors_of_u = {}
-# 			for j in g1.neighbors(i):
-# 				for v in g2.neighbors(u):
-# 					B[(j, v)] = S[j, v]
-# 					neighbors_of_i[j] = 0
-# 					neighbors_of_u[v] = 0
+	# NOTE: for b and b_new, the index of u is u + g1_size
+	for i in g1.nodes():
+		for u in g2.nodes():
+			# ---------------------------------------
+			#    The complete bipartite graph       -
+			# ---------------------------------------
+			# Instead of constructing the actural graph (which is costly), we create a dictionary B with the format:
+			# {(j, v) : weight}
+			# the 0 means this edge has not been deleted
+			B = defaultdict(lambda : 0.0)
+			neighbors_of_i = {}
+			neighbors_of_u = {}
+			for j in g1.neighbors(i):
+				for v in g2.neighbors(u):
+					B[(j, v)] = S[j, v]
+					neighbors_of_i[j] = 0
+					neighbors_of_u[v] = 0
 			
-# 			# format (list of tuples): [ ( (j, v), weight ) ]
-# 			sorted_B = uti.sort_dict(B)
+			# format (list of tuples): [ ( (j, v), weight ) ]
+			sorted_B = uti.sort_dict(B)
 
-# 			index_of_the_largest_undeleted_edge = 0
-# 			c = 0 # accumulated similarity values
-# 			total_number_of_edges = len(list(g1.neighbors(i))) *  len(list(g2.neighbors(u)))
+			index_of_the_largest_undeleted_edge = 0
+			c = 0 # accumulated similarity values
+			total_number_of_edges = len(list(g1.neighbors(i))) *  len(list(g2.neighbors(u)))
 
-# 			while index_of_the_largest_undeleted_edge != total_number_of_edges:
-# 				# print(index_of_the_largest_undeleted_edge)
-# 				# format: ((j, v), [weight, 0])
-# 				largest = sorted_B[index_of_the_largest_undeleted_edge]
-# 				j = largest[0][0]
-# 				v = largest[0][1]
-# 				weight = largest[1]
-# 				if not (weight < b[j,0] and weight < b[v + g1_size, 0]):
-# 					larger = max(b[j,0], b[v + g1_size, 0])
-# 					c += 2 * weight - larger
-# 					neighbors_of_i[j] = 1
-# 					neighbors_of_u[v] = 1
+			while index_of_the_largest_undeleted_edge != total_number_of_edges:
+				# print(index_of_the_largest_undeleted_edge)
+				# format: ((j, v), [weight, 0])
+				largest = sorted_B[index_of_the_largest_undeleted_edge]
+				j = largest[0][0]
+				v = largest[0][1]
+				weight = largest[1]
+				if not (weight < b[j,0] and weight < b[v + g1_size, 0]):
+					larger = max(b[j,0], b[v + g1_size, 0])
+					c += 2 * weight - larger
+					neighbors_of_i[j] = 1
+					neighbors_of_u[v] = 1
 
-# 				index_of_the_largest_undeleted_edge += 1
+				index_of_the_largest_undeleted_edge += 1
 
-# 				while (index_of_the_largest_undeleted_edge != total_number_of_edges) and ((neighbors_of_i[sorted_B[index_of_the_largest_undeleted_edge][0][0]]) or (neighbors_of_u[sorted_B[index_of_the_largest_undeleted_edge][0][1]])):
-# 					index_of_the_largest_undeleted_edge += 1
-					
-# 			maxi = max(len(list(g1.neighbors(i))), len(list(g2.neighbors(u))))
-# 			S_new[i,u] = c / maxi
-# 			if S_new[i,u] > b_new[i]:
-# 				b_new[i] = S_new[i,u]
-# 			if S_new[i,u] > b_new[u + g1_size]:
-# 				b_new[u + g1_size] = S_new[i,u]
-# 	# Update
-# 	b = b_new
-# 	S = S_new
+				while (index_of_the_largest_undeleted_edge != total_number_of_edges) and ((neighbors_of_i[sorted_B[index_of_the_largest_undeleted_edge][0][0]]) or (neighbors_of_u[sorted_B[index_of_the_largest_undeleted_edge][0][1]])):
+					index_of_the_largest_undeleted_edge += 1
+			
+			maxi = max(len(list(g1.neighbors(i))), len(list(g2.neighbors(u))))
+			S_new[i,u] = c / maxi
+			if S_new[i,u] > b_new[i]:
+				b_new[i] = S_new[i,u]
+			if S_new[i,u] > b_new[u + g1_size]:
+				b_new[u + g1_size] = S_new[i,u]
+	# Update
+	b = b_new
+	S = S_new
 
 
 # --------------------------------------
 #                 MAIN                 -
 # --------------------------------------
-# mapping = dict()
-# selected = [0] * (g2_size)
+mapping = dict()
+selected = [0] * (g2_size)
 
-# for i in range(S.shape[0]):
-# 	maxi = -1
-# 	max_index = 0
-# 	for u in range(S.shape[1]):
-# 		if S[i,u] > maxi and not selected[u]:
-# 			maxi = S[i, u]
-# 			max_index = u 
-# 	mapping[i] = max_index
-# 	selected[max_index] = 1
+for i in range(S.shape[0]):
+	maxi = -1
+	max_index = 0
+	for u in range(S.shape[1]):
+		if S[i,u] > maxi and not selected[u]:
+			maxi = S[i, u]
+			max_index = u 
+	mapping[i] = max_index
+	selected[max_index] = 1
 
-# matched_node = 0
-# for i in range(S.shape[0]):
-#     if gt_mapping[i] == mapping[i]: 
-#         matched_node += 1
-# print("Initial mapping percentage: {}".format(matched_node / S.shape[0]))
+matched_node = 0
+for i in range(S.shape[0]):
+    if gt_mapping[i] == mapping[i]: 
+        matched_node += 1
+print("Initial mapping percentage: {}".format(matched_node / S.shape[0]))
 
-# count = 0
-# for i1 in range(len(g1)):
-# 	for j1 in range(len(g1)):
-# 		i2 = mapping[i1]
-# 		j2 = mapping[j1]
-# 		if C[i1, j1] == 1:
-# 			if D[i2, j2] == d_replace:
-# 				count += 1
-# 		if C[i1, j1] == 0:
-# 			if D[i2, j2] == 1:
-# 				count += 1
-# print("Initial violations: {}".format(count))
+count = 0
+for i1 in range(len(g1)):
+	for j1 in range(len(g1)):
+		i2 = mapping[i1]
+		j2 = mapping[j1]
+		if C[i1, j1] == 1:
+			if D[i2, j2] == d_replace:
+				count += 1
+		if C[i1, j1] == 0:
+			if D[i2, j2] == 1:
+				count += 1
+print("Initial violations: {}".format(count))
 
 
-H = np.kron(C, D)
-GG = nx.from_numpy_matrix(H)
-R = nx.pagerank(GG)
+def IsoRank(A1, A2, tol, max_iter, H = None):
+	# Shapes of two matrices
+	n1 = A1.shape[0]
+	n2 = A2.shape[0]
+
+	# Normalization of adjacency matrix
+	row_sum_1 = np.zeros((n1, 1), np.float64)
+	row_sum_2 = np.zeros((n2, 1), np.float64)
+
+	for i in range(n1):
+		row_sum_1[i, 0] = 1/np.sum(A1[i])
+		row_sum_2[i, 0] = 1/np.sum(A2[i])
+
+	W1 = np.multiply(row_sum_1, A1)
+	W2 = np.multiply(row_sum_2, A2)
+
+	# Inilization of similarity matrix
+	S = np.full((n2, n1), 1/(n1 * n2))
+
+	# Prior similarity matrix
+	if not prior:
+		H = S
+
+	# Main iterations
+	for i in range(max_iter):
+		prev = S
+		W2_t = np.transpose(W2)
+		M1 = np.matmul(W2_t, S)
+		M2 = np.matmul(M1, W1)
+		S = 0.5 * M2 + 0.5 * H
+		delta = np.linalg.norm(S - prev)
+		if delta < 0.000005:
+			print("Total number of iterations: {}".format(i))
+			break
+
+	return S
