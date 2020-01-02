61f94bc685bfb2b17794998e6f7a665b1e352854
BridgelessAlexQiu <zqgraph@gmail.com>
2020-01-02 17:48:05 -0500

    netal

---
+++ code/c/netal/NETAL.cpp
@@ -0,0 +1,198 @@
+#include "Alignment.h"
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	double aa = 0.0001, bb = 0, cc = 1; // weighting parameters in the paper
+    
+	int it = 2;     // Number of iterations when calculating the similarity
+	int rr = 0;     // The type of randomness
+	int pp = 0;     // The percentage of randomness
+	int nn = 1;     // The number of random networks
+	
+    char* name1;    // The name of the first network
+    char* name2;    // The name of second network
+
+	try
+	{
+		
+        if(argc < 3) {
+            cout << "There should be two files as input!" <<endl;
+            return -1;
+        }
+        else //input arguments
+        {
+            int i = 1; //counter for input parameters
+            name1 = argv[ i++ ]; //first network
+            name2 = argv[ i++ ]; //second network
+            
+            while (i<argc) //check all the input parameters
+            {
+                if ( ( strlen(argv[i]) == 2 ) && ( argv[i][0]=='-' ) && ( argv[i][1]=='a' || argv[i][1]=='b' || argv[i][1]=='c' || argv[i][1]=='i' ||argv[i][1]=='r' ||argv[i][1]=='n') && ( i + 1 < argc) )
+                {
+                    i++;
+                    if( argv[i-1][1]=='a' )
+                    {
+                        aa = atof(argv[i]);
+                        if( aa <0 || aa > 1)
+                        {
+                            cout << "Error : value of a must be between 0 and 1 :" << endl;
+                            return -1;
+                        }
+                    }
+                    else if( argv[i-1][1]=='b' )
+                    {
+                        bb = atof(argv[i]);
+                        if( bb <0 || bb > 1)
+                        {
+                            cout << "Error : value of b must be between 0 and 1 :" << endl;
+                            return -1;
+                        }
+                    }
+                    else if( argv[i-1][1]=='c' )
+                    {
+                        cc = atof(argv[i]);
+                        if( cc <0 || cc > 1)
+                        {
+                            cout << "Error : value of c must be between 0 and 1 :" << endl;
+                            return -1;
+                        }
+                    }
+                    else if( argv[i-1][1]=='n' )
+                    {
+                        nn = atoi(argv[i]);
+                    }
+                    else if( argv[i-1][1]=='r' )
+                    {
+                        rr = atoi(argv[i]);
+                        i++;
+                        pp = atoi(argv[i]);
+                    }
+                    else
+                    {
+                        it = atoi(argv[i]);
+                        if( it < 0 )
+                        {
+                            cout << "Error : value of i must be positive" << endl;
+                            return -1;
+                        }
+                    }
+                    i++;
+                }
+                else
+                {
+                    cout << "Error in argument : " << argv[i] << endl;
+                    return -1;
+                }
+            } //end while
+
+            //construct the first network
+            Network network1(name1);
+ 
+            //construct the first network
+            Network network2(name2);
+            
+            bool reverse = false; //it means size of first input network is bigger than second one
+            
+            if(network1.size > network2.size)
+                reverse = true;
+            
+            //Initializes the alignment class
+            Alignment alignment( network1, network2 );
+            
+            //Calculates the similarity values
+            alignment.setSimilarities(it, bb, cc);
+            
+            //Aligns two networks
+            alignment.align(aa);
+            
+            //calculation of evaluating measures
+            int CCCV = 0, CCCE = 0; //LCCS based on vertex and edge
+            float EC = 0, NC = 0, IC = 0, S3 = 0;
+            CCCV += alignment.CCCV;
+            CCCE += alignment.CCCE;
+            EC += alignment.EC;
+            NC += alignment.NC;
+            IC += alignment.IC;
+            S3 += alignment.S3;
+
+            cout<<"EC: "<<EC<<endl;
+            cout<<"S3: "<<S3<<endl;
+            
+            //making the name for output file
+            stringstream strm;
+            strm << "(" << name1 << "-" << name2;
+            if( rr > 0 )
+                strm << rr << "r" << pp;
+            strm << ")" << "-a" << aa << "-b" << bb << "-c" << cc << "-i" << it;
+            
+            //no random alignment
+            if(rr == 0)
+            {
+                alignment.outputEvaluation(strm.str());
+                alignment.outputAlignment(strm.str());
+            }
+            //random alignment
+            else
+            {
+                int edgeNum; //percent of randome edges should be removed
+                
+                for( int i = 1; i < nn; i++ )
+                {
+                    
+                    //cout << "-------------------------------> Random Netowrk:  " << i << endl; //print the iteration number
+                    Network network2(name2);
+                    edgeNum = pp * network2.numOfEdge / 100;
+                    
+                    if( rr == 1) //construct the random graph with removing edges randomly
+                    {
+                        network2.randomEdgeRemoval( edgeNum );
+                    }
+                    else if(rr == 2) //construct the random graph with adding edges randomly
+                    {
+                        network2.randomEdgeAddition( edgeNum );
+                    }
+                    else if(rr == 3) //construct the random graph with removing and adding edges randomly
+                    {
+                        network2.randomEdgeRemoval( edgeNum );
+                        network2.randomEdgeAddition( edgeNum );
+                    }
+                    
+                    //find the alignment
+                    Alignment alignment( network1, network2 );
+                    alignment.setSimilarities(it, bb, cc);
+                    alignment.align(aa);
+                    
+                    //calculate the eavluation measurements
+                    CCCV += alignment.CCCV;
+                    CCCE += alignment.CCCE;
+                    EC += alignment.EC;
+                    NC += alignment.NC;
+                    IC += alignment.IC;
+                    
+                    //print the results in files
+                    alignment.outputEvaluation(strm.str());
+                    alignment.outputAlignment(strm.str());
+                }
+            
+            }
+        }
+    }
+    catch(exception &e)
+    {
+        cout << "Error in arguments or input files!" << endl;
+        e.what();
+        return -1;
+    }
+        
+    return 0;
+}
+
+
+
