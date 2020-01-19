d0266408464df009790133641869c57d8483a799
Zirou Qiu <zqgraph@gmail.com>
2020-01-19 15:51:04 -0500

    new

--- code/c/datasets/homogeneous/digg/split_network.py
+++ code/c/datasets/homogeneous/digg/split_network.py
@@ -12,7 +12,7 @@ print("\n")
 #       Induced Subgraph      #
 # --------------------------- #
 node_subset_1 = list(range(2500, 7500))
-node_subset_2 = list(range(3500, 7500))
+node_subset_2 = list(range(3500, 8500))
 
 g1 = G_full.subgraph(node_subset_1)
 g2 = G_full.subgraph(node_subset_2)
