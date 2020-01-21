18d2a075628c32885ba621900b63c9cd8f48ed0f
Zirou Qiu <zqgraph@gmail.com>
2020-01-21 12:42:19 -0500

    code for computing diameter added

---
+++ code/c/datasets/heterogeneous/flickr_lastfm/compute_diameter.py
@@ -0,0 +1,9 @@
+import networkx as nx
+
+# Read in networks
+g1 = nx.read_edgelist("flickr.edges")
+g2 = nx.read_edgelist("lastfm.edges")
+
+# Print out diameters
+print("Diameter of G1: {}".format(nx.diameter(g1))) 
+print("Diameter of G2: {}".format(nx.diameter(g2)))
