96ac3206ad4ab6cec79abd4908c85cc77188517f
Zirou Qiu <zqgraph@gmail.com>
2020-01-21 09:43:58 -0500

    minor update

--- code/c/rulealign/main.cpp
+++ code/c/rulealign/main.cpp
@@ -530,8 +530,12 @@ int main(int argc, char* argv[])
 				else
 				{
 					S_new[i][u] = c / maxi;
-					if(S_new[i][u] < -1) cout<<S_new[i][u]<<endl;
-					S_new[i][u] = -1;
+					if(S_new[i][u] < -1)
+					{
+						cout<<S_new[i][u]<<endl;
+						S_new[i][u] = -1;
+					} 
+					
 				}
 
 				// ------------------------------------------
